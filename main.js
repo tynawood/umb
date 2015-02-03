01734a3c802cebfbefeb28dc91e7aae46b38a75a
tynawood <wood.tyna@gmail.com>
2015-02-03 02:27:55 -0800

    ffhaksjfhkaf
    fafafjasfjaafkjfafla

---
+++ main.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">var app = require('app');
+var BrowserWindow = require('browser-window');
+var onlineStatusWindow;
+
+app.on('ready', function() {
+  onlineStatusWindow = new BrowserWindow({ width: 0, height: 0, show: false });
+  onlineStatusWindow.loadUrl('file://' + __dirname + '/main.html');
+});
+
+</script>
